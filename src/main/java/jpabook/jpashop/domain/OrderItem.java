401b48d4ccf593ebe219c211013db56f0ca8c057
kongil <o_o0122@naver.com>
2023-03-04 13:53:23 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "ODER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ITEM_ID")
+    private Long itemId;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private int price;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
